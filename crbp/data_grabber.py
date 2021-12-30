098b9e8dbc6dfef44eef678882f0c6d9925e7b04
Eric Xue <widefin@gmail.com>
2021-12-30 17:31:21 -0500

    create better card indexing

--- crbp/data_grabber.py
+++ crbp/data_grabber.py
@@ -11,18 +11,46 @@ url = "https://api.clashroyale.com/v1/"
 # LOCATIONS (inclusive): 57000001-57000006 NON COUNTRIES, 57000007-57000260 COUNTRIES
 # USA: 57000249
 
+def translate_id(id):
+    id -= 26000000
+    id_change = 0
+
+    if id < 1000000:
+        if id >= 67:
+            id_change -= 2
+        if id >= 72:
+            id_change -= 2
+        if id >= 74:
+            id_change -= 1
+        if id >= 80:
+            id_change -= 5
+        if id >= 83:
+            id_change -= 2
+    elif id < 2000000:
+        id -= 999926
+        if id >= 86:
+           id_change -= 1
+    else:
+        id -= 1999913
+
+    return id + id_change
+
 def encode_battle(battle, card_list):
     cards = battle["team"][0]["cards"]
     output = [0] * 213
+
     for card in cards:
-        id = int(card["id"]) - 26000000
-        output[id if id < 1000000 else (id - 999926 if id < 2000000 else id - 1999912)] += 1
-        card_list[id if id < 1000000 else (id - 999926 if id < 2000000 else id - 1999912)] = card["name"]
+        id = translate_id(int(card["id"]))
+        output[id] += 1
+        card_list[id] = card["name"]
+
     cards = battle["opponent"][0]["cards"]
     for card in cards:
-        id = int(card["id"]) - 26000000
-        output[(id if id < 1000000 else (id - 999926 if id < 2000000 else id - 1999912))+105] += 1
-        card_list[(id if id < 1000000 else (id - 999926 if id < 2000000 else id - 1999912))+105] = card["name"]
+        id = translate_id(int(card["id"])) + 106
+
+        output[id] += 1
+        card_list[id] = card["name"]
+
     crown_difference = battle["team"][0]["crowns"] - battle["opponent"][0]["crowns"]
     output[212] = 1 if crown_difference > 0 else 0
     return output, card_list
