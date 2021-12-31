fb9e3bcd9b27a5f58173177e472969c04da9263b
Eric Xue <widefin@gmail.com>
2021-12-31 13:51:39 -0500

    added visualisation through t SNE

--- crbp/main.py
+++ crbp/main.py
@@ -1,21 +1,38 @@
 import pandas as pd
-from sklearn import svm
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
-import tqdm
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tqdm.keras import TqdmCallback
 from matplotlib import pyplot as plt
 
-battles_df = pd.read_csv("../data/data.csv").sample(frac=1)
+battles_df = pd.read_csv("../data/data.csv")
 X = battles_df.iloc[:,1:213]
 y = battles_df.iloc[:,213]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-clf = svm.SVC()
-clf.fit(X_train, y_train)
+model = Sequential()
 
-y_pred = clf.predict(X_train)
-print("Accuracy:",metrics.accuracy_score(y_train, y_pred))
+model.add(Dense(units = 144, activation = "relu"))
+model.add(Dropout(0.1))
+model.add(Dense(units = 144, activation = "relu"))
+model.add(Dropout(0.1))
+model.add(Dense(units = 144, activation = "relu"))
+model.add(Dropout(0.1))
+model.add(Dense(units = 144, activation = "relu"))
+model.add(Dropout(0.1))
+model.add(Dense(units = 144, activation = "relu"))
+model.add(Dropout(0.1))
+model.add(Dense(units = 144, activation = "relu"))
+model.add(Dropout(0.1))
+model.add(Dense(units = 144, activation = "relu"))
+model.add(Dropout(0.1))
+model.add(Dense(units = 1, activation = "sigmoid"))
 
-y_pred = clf.predict(X_test)
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+model.compile(loss = "binary_crossentropy", optimizer = tf.keras.optimizers.SGD(learning_rate = 0.00075), metrics = ["accuracy"])
+history = model.fit(X_train, y_train, batch_size = 1024, epochs = 2000, shuffle = True, verbose=0, callbacks=[TqdmCallback(verbose=0)], validation_split=0.2)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.show()
