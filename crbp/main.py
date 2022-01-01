f8bd7ca7a3d96548b4f1b5d6cad64bf37c182918
Eric Xue <widefin@gmail.com>
2022-01-01 17:11:06 -0500

    detailed trials

--- crbp/main.py
+++ crbp/main.py
@@ -1,30 +1,35 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.feature_selection import SelectKBest, chi2
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tqdm.keras import TqdmCallback
-from matplotlib import pyplot as plt
+import xgboost as xgb
+
 
 battles_df = pd.read_csv("../data/data.csv")
 X = battles_df.iloc[:,1:213]
 y = battles_df.iloc[:,213]
 
-Xt = SelectKBest(chi2, k=106).fit_transform(X, y)
-X_train, X_test, y_train, y_test = train_test_split(Xt, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = Sequential()
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dtest = xgb.DMatrix(X_test, label=y_test)
 
-model.add(Dense(units = 96, activation = "relu"))
-model.add(Dense(units = 96, activation = "relu"))
-model.add(Dense(units = 96, activation = "relu"))
-model.add(Dense(units = 96, activation = "relu"))
-model.add(Dense(units = 1, activation = "sigmoid"))
+params = {
+    "max_depth":7,
+    "min_child_weight": 12,
+    "eta": 0.2,
+    "subsample": 1,
+    "colsample_bytree": 0.92,
+    "objective": "binary:logistic"
+}
 
-model.compile(loss = "binary_crossentropy", optimizer = tf.keras.optimizers.SGD(learning_rate = 0.001), metrics = ["accuracy"])
-history = model.fit(X_train, y_train, batch_size = 1024, epochs = 100, shuffle = True, verbose=0, callbacks=[TqdmCallback(verbose=0)], validation_split=0.2)
+params['eval_metric'] = "error"
+num_boost_round = 5000
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.show()
+model = xgb.train(
+    params,
+    dtrain,
+    num_boost_round=num_boost_round,
+    evals=[(dtest, "Test")],
+    early_stopping_rounds=10
+)
+
+print("Best MAE: {} in {} rounds".format(model.best_score, model.best_iteration+1))
