2828d24b1ab4597deba9c066f669119173150684
Eric Xue <widefin@gmail.com>
2021-12-31 22:56:00 -0500

    neural net with feature extraction

--- crbp/main.py
+++ crbp/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, chi2
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
@@ -10,28 +11,19 @@ battles_df = pd.read_csv("../data/data.csv")
 X = battles_df.iloc[:,1:213]
 y = battles_df.iloc[:,213]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+Xt = SelectKBest(chi2, k=106).fit_transform(X, y)
+X_train, X_test, y_train, y_test = train_test_split(Xt, y, test_size=0.2)
 
 model = Sequential()
 
-model.add(Dense(units = 144, activation = "relu"))
-model.add(Dropout(0.1))
-model.add(Dense(units = 144, activation = "relu"))
-model.add(Dropout(0.1))
-model.add(Dense(units = 144, activation = "relu"))
-model.add(Dropout(0.1))
-model.add(Dense(units = 144, activation = "relu"))
-model.add(Dropout(0.1))
-model.add(Dense(units = 144, activation = "relu"))
-model.add(Dropout(0.1))
-model.add(Dense(units = 144, activation = "relu"))
-model.add(Dropout(0.1))
-model.add(Dense(units = 144, activation = "relu"))
-model.add(Dropout(0.1))
+model.add(Dense(units = 96, activation = "relu"))
+model.add(Dense(units = 96, activation = "relu"))
+model.add(Dense(units = 96, activation = "relu"))
+model.add(Dense(units = 96, activation = "relu"))
 model.add(Dense(units = 1, activation = "sigmoid"))
 
-model.compile(loss = "binary_crossentropy", optimizer = tf.keras.optimizers.SGD(learning_rate = 0.00075), metrics = ["accuracy"])
-history = model.fit(X_train, y_train, batch_size = 1024, epochs = 2000, shuffle = True, verbose=0, callbacks=[TqdmCallback(verbose=0)], validation_split=0.2)
+model.compile(loss = "binary_crossentropy", optimizer = tf.keras.optimizers.SGD(learning_rate = 0.001), metrics = ["accuracy"])
+history = model.fit(X_train, y_train, batch_size = 1024, epochs = 100, shuffle = True, verbose=0, callbacks=[TqdmCallback(verbose=0)], validation_split=0.2)
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
