4a945500cf6ba696f9c311d4162fb7a8e138f37c
Eric Xue <widefin@gmail.com>
2022-01-05 11:58:01 -0500

    added model saving and model evaluation after training

--- crbp/main.py
+++ crbp/main.py
@@ -8,46 +8,61 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.regularizers import L1
 from tqdm.keras import TqdmCallback
 from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
 
+battles_df = pd.read_csv("../data/data.csv")
 
-battles_df = pd.read_csv("../data/data.csv").sample(frac=1)
-X = battles_df.iloc[:,1:215]
-y = battles_df.iloc[:,215]
-
-outliers = []
+outliers = [] 
 
 for i, row in battles_df.iterrows():
     diff = abs(row[213] - row[214])
     if diff > 1000:
         outliers.append(i)
 
-battles_df.drop(battles_df.index[outliers])    
+battles_df.drop(battles_df.index[outliers])   
+
+X = battles_df.iloc[:,1:215]
+y = battles_df.iloc[:,215]
 
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model = Sequential()
-model.add(Dense(144, input_dim=214, activation="relu", kernel_regularizer=L1(0.0001)))
-model.add(Dense(144, activation="relu", kernel_regularizer=L1(0.0001)))
+model.add(Dense(144, input_dim=214, activation="relu", kernel_regularizer=L1(0.0001), kernel_initializer="he_uniform"))
+model.add(Dense(144, activation="relu", kernel_regularizer=L1(0.0001), kernel_initializer="he_uniform"))
 model.add(Dense(1, activation="sigmoid", kernel_regularizer=L1(0.0001)))
 model.summary()
 
 model.compile(
-    optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.00025),
     loss="binary_crossentropy",
     metrics=["accuracy"]
 )
 epochs_hist = model.fit(
     X_train,
     y_train,
-    epochs=256,
-    batch_size=64,
+    shuffle=True,
+    epochs=512,
+    batch_size=128,
     verbose=0,
     validation_split=0.2,
-    callbacks=[TqdmCallback(), EarlyStopping(monitor="val_loss", patience=5)]
+    callbacks=[TqdmCallback(), EarlyStopping(monitor="val_loss", patience=10)]
 )
 
 plt.plot(epochs_hist.history['loss'])
 plt.plot(epochs_hist.history['val_loss'])
 plt.show()
+
+evaluation = model.evaluate(X_test, y_test)
+print("Test Accuracy: {}".format(evaluation[1]))
+y_pred = model.predict_classes(X_test) 
+
+tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+print("True Negative: ", tn)
+print("False Positive: ", fp)
+print("False Negative: ", fn)
+print("True Positive: ", tp)
+
+if input("Save model? (y/n)") == "y":
+    model.save("../models/trial2.h5")
